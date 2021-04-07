d45f97fa0f44c022b587a282cbe17a07b1ca256a
Alex3098 <uovwbo@gmail.com>
2021-04-07 16:40:27 +0300

    Final version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
-import Weather from './components/Weather';
+import React, { useEffect, useState } from "react";
+import Weather from "./components/Weather";
 
 function App() {
+  const [temperature, setTemperature] = useState();
+  const [city, setCity] = useState();
+  const [country, setCountry] = useState();
+  const [weatherType, setWeatherType] = useState();
+  const [clouds, setClouds] = useState();
+  const [humidity, setHumidity] = useState();
+  const [wind, setWind] = useState();
+  const [error, setError] = useState();
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  useEffect(() => {
+    async function fetchData() {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_call.json();
+
+      setTemperature(data.main.temp.toFixed(0));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setWeatherType(data.weather[0].main);
+      setClouds(data.clouds.all);
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed.toFixed());
+    }
+    fetchData();
+  }, []);
+
+  const getWeather = async (e) => {
+    e.preventDefault();
+    try {
+      const city = e.target.elements.city.value;
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_call.json();
+
+      setTemperature(data.main.temp.toFixed(0));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setWeatherType(data.weather[0].main);
+      setClouds(data.clouds.all);
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed.toFixed());
+      setError("");
+    } catch (e) {
+      setError("City not found");
+    }
+  };
+
   return (
     <div>
-      <Weather />
+      <Weather
+        getWeather={getWeather}
+        temperature={temperature}
+        city={city}
+        country={country}
+        weatherType={weatherType}
+        clouds={clouds}
+        humidity={humidity}
+        wind={wind}
+        error={error}
+      />
     </div>
   );
-
 }
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./Weather.module.css";
 import sunny from "../img/sunny.svg";
 import rain from "../img/rain.svg";
 import cloudy from "../img/cloudy.svg";
+import snow from "../img/snow.svg";
+import thunder from "../img/thunder.svg";
 import sunnyBackground from "../img/sunny-background.jpg";
 import rainyBackground from "../img/rainy-background.jpg";
 import cloudyBackground from "../img/cloudy-background.jpg";
+import snowBackground from "../img/snow-background.jpg";
+import thunderBackground from "../img/thunder-background.jpg";
+import loupe from "../img/loupe.svg";
 
-export default function Weather() {
-  const [weatherPhoto, setWeatherPhoto] = useState(rainyBackground);
-  const [weatherIcon, setWeatherIcon] = useState(sunny);
+export default function Weather(props) {
+  const [weatherPhoto, setWeatherPhoto] = useState();
+  const [weatherIcon, setWeatherIcon] = useState();
+  useEffect(() => {
+    if (props.weatherType === "Clear") {
+      setWeatherPhoto(sunnyBackground);
+      setWeatherIcon(sunny);
+    }
+    if (props.weatherType === "Rain" || props.weatherType === "Drizzle") {
+      setWeatherPhoto(rainyBackground);
+      setWeatherIcon(rain);
+    }
+    if (props.weatherType === "Clouds" || props.weatherType === "Atmosphere") {
+      setWeatherPhoto(cloudyBackground);
+      setWeatherIcon(cloudy);
+    }
+    if (props.weatherType === "Snow") {
+      setWeatherPhoto(snowBackground);
+      setWeatherIcon(snow);
+    }
+    if (props.weatherType === "Thunderstorm") {
+      setWeatherPhoto(thunderBackground);
+      setWeatherIcon(thunder);
+    }
+  });
+  const getCurrentDate = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
+    return `${day}, ${date}, ${month}, ${year}`;
+  };
   return (
     <div className={style.container}>
       <div
@@ -24,16 +81,18 @@ export default function Weather() {
         >
           <div className={style.logo}>the.weather</div>
           <div className={style.main_info_block_text}>
-            <div className={style.temp}>26°</div>
+            <div className={style.temp}>{props.temperature}°</div>
             <div className={style.city_time_block}>
-              <div className={style.city}>London</div>
-              <div className={style.time}>10:36 - Tuesday, 22 Oct '19</div>
+              <div className={style.city}>
+                {props.city},{props.country}
+              </div>
+              <div className={style.time}>{getCurrentDate(new Date())}</div>
             </div>
             <div className={style.type_block}>
               <div className={style.type_icon}>
                 <img src={weatherIcon} alt="sunny" />
               </div>
-              <div className={style.type_text}>Sunny</div>
+              <div className={style.type_text}>{props.weatherType}</div>
             </div>
           </div>
         </div>
@@ -41,38 +100,28 @@ export default function Weather() {
         <div className={style.other_info}>
           <div className={style.other_info_wrapper}>
             <div className={style.search_block}>
-              <input type="text" placeholder="Another location" />
-              <button>Search</button>
+              <form onSubmit={props.getWeather}>
+                <input type="text" name="city" placeholder="Another location" />
+                <button>
+                  <img src={loupe} alt="Loupe icon" />
+                </button>
+              </form>
             </div>
-
-            <ul>
-              <li>Birmingham</li>
-              <li>Manchester</li>
-              <li>New York</li>
-              <li>California</li>
-            </ul>
-
-            {/* <hr /> */}
+            <div className={style.error_city}>{props.error}</div>
 
             <h3>Weather Details</h3>
             <div className={style.cloudy_info}>
               <h4>Cloudy</h4>
-              <p>12%</p>
+              <p>{props.clouds}%</p>
             </div>
             <div className={style.humidity_info}>
               <h4>Humidity</h4>
-              <p>78%</p>
+              <p>{props.humidity}%</p>
             </div>
             <div className={style.wind_info}>
               <h4>Wind</h4>
-              <p>1km/h</p>
+              <p>{props.wind}km/h</p>
             </div>
-            <div className={style.rain_info}>
-              <h4>Rain</h4>
-              <p>0mm</p>
-            </div>
-
-            {/* <hr /> */}
           </div>
         </div>
       </div>
